8b2c231cb5ea84177613ce450b75be2cae22f5a5
sebong Oh <osb4226@gmail.com>
2021-02-09 19:09:27 +0900

    [add] HelloController

--- src/main/java/com/bong/book/springboot/web/HelloController.java
+++ src/main/java/com/bong/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.bong.book.springboot.web;
 
+import com.bong.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,12 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+
+        return new HelloResponseDto(name,amount);
+    }
+
 
 }
