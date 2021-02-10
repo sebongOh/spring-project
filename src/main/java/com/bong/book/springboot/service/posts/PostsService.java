d06d97adec0bd3acda86ba53278dcc0593d60c1f
sebong Oh <osb4226@gmail.com>
2021-02-10 18:43:20 +0900

    [add] PostsService

---
+++ src/main/java/com/bong/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,37 @@
+package com.bong.book.springboot.service.posts;
+
+import com.bong.book.springboot.domain.posts.Posts;
+import com.bong.book.springboot.domain.posts.PostsRepository;
+import com.bong.book.springboot.web.dto.PostsResponseDto;
+import com.bong.book.springboot.web.dto.PostsSaveRequestDto;
+import com.bong.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("해당 사용자가 없습니다. id ="+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("해당 사용자가 없습니다. id = "+id));
+        return new PostsResponseDto(posts);
+    }
+
+}
