0ca8486636c73e9ee3759010fc93cb553d9e73b5
sebong Oh <osb4226@gmail.com>
2021-02-10 18:43:34 +0900

    [add] Posts Controller

---
+++ src/main/java/com/bong/book/springboot/web/PostApiController.java
@@ -0,0 +1,32 @@
+package com.bong.book.springboot.web;
+
+import com.bong.book.springboot.service.posts.PostsService;
+import com.bong.book.springboot.web.dto.PostsResponseDto;
+import com.bong.book.springboot.web.dto.PostsSaveRequestDto;
+import com.bong.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor //final로 선언된 인자 생성자 역할
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id , @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+}
