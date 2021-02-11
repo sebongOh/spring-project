3a72f5f304f2c1b09124531e57729ddbb40a8bba
sebong Oh <osb4226@gmail.com>
2021-02-11 17:41:22 +0900

    [add] 게시글 crud

--- src/main/java/com/bong/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/bong/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.bong.book.springboot.service.posts;
 
 import com.bong.book.springboot.domain.posts.Posts;
 import com.bong.book.springboot.domain.posts.PostsRepository;
+import com.bong.book.springboot.web.dto.PostsListResponseDto;
 import com.bong.book.springboot.web.dto.PostsResponseDto;
 import com.bong.book.springboot.web.dto.PostsSaveRequestDto;
 import com.bong.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,6 +37,13 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(()->
                 new IllegalArgumentException("해당 사용자가 없습니다. id = "+id));
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
