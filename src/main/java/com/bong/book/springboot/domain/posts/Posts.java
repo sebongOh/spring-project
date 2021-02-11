be48c877be74854458cf7593cfbca56204c04cb9
sebong Oh <osb4226@gmail.com>
2021-02-11 17:38:59 +0900

    [add] BaseTime 상속

--- src/main/java/com/bong/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/bong/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.bong.book.springboot.domain.posts;
 
+import com.bong.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
 
     @Id
